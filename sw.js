f5c350848d93138fe93b70ad43fbff6d88588ff5
PeterFalconi <walterbruno699@gmail.com>
2025-07-18 13:00:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service Worker per cache offline
+const CACHE_NAME = 'orologio-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
